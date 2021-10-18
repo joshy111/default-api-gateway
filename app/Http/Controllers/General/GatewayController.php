6a6ec23f3c9b6ecc76c050f4e88beb5fb301ca87
joshy111 <79142183+joshy111@users.noreply.github.com>
2021-10-18 14:48:33 +0100

    Perfected API

--- app/Http/Controllers/General/GatewayController.php
+++ app/Http/Controllers/General/GatewayController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\General;
 
 use App\Http\Controllers\Controller;
 use App\Models\Gateway;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 
 class GatewayController extends Controller
@@ -18,20 +19,28 @@ class GatewayController extends Controller
         $api_name = $request->api_name; 
 
         // Used to validate Request
-        // $token = $request->token;
-        $token = '$2y$10$V9SLEafi3QeSaC9dpiDeo.njdM805jVuVjMdCQzM023GoR2J7ztGO';
+        $token = $request->token;
+        // $token = '$2y$10$V9SLEafi3QeSaC9dpiDeo.njdM805jVuVjMdCQzM023GoR2J7ztGO';
 
-        // Incase payload is being sent || For POST requests
+        // Incase payload is being sent || For POST requests ** Be sure to change requests to data in all API's 
+        // i.e from $request->query to $request->data->query
         $data = $request->data;
 
-        $gateway = Gateway::where('API_NAME', $api_name)->get();
+        $gateway = Gateway::select(['API_NAME', 'API_URL', 'API_METHOD', 'API_TOKEN'])->where('API_NAME', $api_name)->first();
 
-        # Validate Token
-        if(!$gateway->API_TOKEN == $token){
+        if(!$gateway){
             return;
         }
 
-        # Make Call to API
+        // return $gateway;
+
+        # Validate Token
+        if(!$token == $gateway->API_TOKEN){
+            return;
+        }
+
+        
+        // # Make Call to API
         $result = $this->forward($gateway->API_URL, $gateway->API_METHOD, $data);
 
         return $result;
@@ -39,10 +48,15 @@ class GatewayController extends Controller
     }
 
     protected function forward($api_url, $api_method, $data){
-        $client = new \GuzzleHttp\Client();
 
-        $response = $client->request("$api_method", $api_url, [$data]);
+        $response = Http::withOptions([
+            'verify' => false
+        ])->$api_method($api_url, [
+            'data' => $data,
+        ]);
 
-        return $response;
+        return $response->getBody();
     }
+
+    
 }
